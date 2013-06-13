c7470ac8d167018217eaa1b7b6bbe1d0e6b11997
ldong <li.dong@rea-group.com>
2013-06-13 22:02:20 +0800

    init website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GREWebsite::Application.config.secret_token = '2a2accd82de553759bc9a2c7c796383d327c4777b8ac0760a0badab4f49d001b03893998a80a4f111aa41c3c8597520727e82bb1c7bcd3670791bea635ea0dfc'
